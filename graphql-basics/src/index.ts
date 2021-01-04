6465d2712dc65201b665fa35c0e07aae5f81a5b6
Bernardo-Quina <bernardoquina@gmail.com>
2021-01-04 22:13:22 +0000

    Comment type & setting its relation with the other types

--- graphql-basics/src/index.ts
+++ graphql-basics/src/index.ts
@@ -7,6 +7,7 @@ type User = {
   email: string
   age?: number
   posts: string[]
+  comments: string[]
 }
 
 // Typescript type for posts
@@ -16,6 +17,15 @@ type Post = {
   body: string
   published: boolean
   author: string
+  comments: string[]
+}
+
+// Typescript type for comments
+type Comment = {
+  id: string
+  text: string
+  author: string
+  post: string
 }
 
 // Demo post data
@@ -25,21 +35,24 @@ const posts: Post[] = [
     title: 'The first post',
     body: 'This is the body of the first post',
     published: true,
-    author: '1'
+    author: '1',
+    comments: ['4', '3']
   },
   {
     id: '2',
     title: 'The second post is not published',
     body: 'This text is not readable by users because it is not published',
     published: false,
-    author: '1'
+    author: '1',
+    comments: ['2']
   },
   {
     id: '3',
     title: 'GraphQL course',
     body: 'I am taking the GraphQL course by Andrew Mead on Udemy',
     published: true,
-    author: '2'
+    author: '2',
+    comments: ['1']
   },
 ]
 
@@ -50,27 +63,59 @@ const users: User[] = [
     name: 'Bernardo',
     email: 'bernardo@example.com',
     age: 24,
-    posts: ['1', '2']
+    posts: ['1', '2'],
+    comments: ['3']
   },
   {
     id: '2',
     name: 'Andrew',
     email: 'andrew@example.com',
     age: 27,
-    posts: ['3']
+    posts: ['3'],
+    comments: ['2']
   },
   {
     id: '3',
     name: 'Sarah',
     email: 'sarah@example.com',
-    posts: []
+    posts: [],
+    comments: ['1']
   },
   {
     id: '4',
     name: 'Mike',
     email: 'mike@example.com',
-    posts: []
+    posts: [],
+    comments: ['4']
   },
+]
+
+// Demo comment data
+const comments: Comment[] = [
+  {
+    id: '1',
+    text: 'This post is trash',
+    author: '3',
+    post: '3'
+  },
+  {
+    id: '2',
+    text: 'I do not find this useful at all',
+    author: '2',
+    post: '2'
+  },
+  {
+    id: '3',
+    text: 'I think this is great!',
+    author: '1',
+    post: '1'
+  },
+  {
+    id: '4',
+    text: 'Just wow!',
+    author: '4',
+    post: '1'
+  }
 ]
 
 // Type definitions (Schema)
@@ -78,6 +123,7 @@ const typeDefs = `
   type Query {
     users(query: String): [User!]!
     posts(query: String): [Post!]!
+    comments: [Comment!]!
     me: User!
     post: Post!
   }
@@ -88,6 +134,7 @@ const typeDefs = `
     email: String!
     age: Int
     posts: [Post!]!
+    comments: [Comment!]!
   }
 
   type Post {
@@ -96,6 +143,14 @@ const typeDefs = `
     body: String!
     published: Boolean!
     author: User!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    text: String!
+    author: User!
+    post: Post!
   }
 `
 
@@ -123,6 +178,9 @@ const resolvers = {
         )
       })
     },
+    comments(parent: any, args: any, ctx: any, info: any) {
+      return comments
+    },
     me() {
       return {
         id: 'abc123',
@@ -145,12 +203,34 @@ const resolvers = {
       return users.find((user) => {
         return user.id === parent.author
       })
+    },
+    comments(parent: Post, args: any, ctx: any, info: any) {
+      return comments.filter((comment) => {
+        return comment.post === parent.id
+      })
     }
   },
   User: {
     posts(parent: User, args: any, ctx: any, info: any) {
       return posts.filter((post) => {
         return post.author === parent.id
+      })
+    },
+    comments(parent: User, args: any, ctx: any, info: any) {
+      return comments.filter((comment) => {
+        return comment.author === parent.id
+      })
+    }
+  },
+  Comment: {
+    author(parent: Comment, args: any, ctx: any, info: any) {
+      return users.find((user) => {
+        return user.id === parent.author
+      })
+    },
+    post(parent: Comment, args: any, ctx: any, info: any) {
+      return posts.find((post) => {
+        return post.id === parent.post
       })
     }
   }
