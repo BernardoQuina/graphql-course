7469dc8775c9ff5e0f1c0c34b67e9d014b06fabc
Bernardo-Quina <bernardoquina@gmail.com>
2021-01-13 19:48:06 +0000

    remove nonNull fields for users query

--- graphql-prisma/src/schema/query.ts
+++ graphql-prisma/src/schema/query.ts
@@ -1,9 +1,6 @@
 import { queryType } from 'nexus'
 
 export const Query = queryType({
-  nonNullDefaults: {
-    input: true,
-  },
   definition(t) {
     t.crud.user()
     t.crud.users({ pagination: true, filtering: true, ordering: true })
