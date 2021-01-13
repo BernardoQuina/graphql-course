23cdf69326222085c27be95271584addd78a9c8d
Bernardo-Quina <bernardoquina@gmail.com>
2021-01-13 17:49:02 +0000

    Add comments to makeschema & other config instances

--- graphql-prisma/src/schema/schema.ts
+++ graphql-prisma/src/schema/schema.ts
@@ -7,6 +7,8 @@ import { User, createUser, deleteUser, updateUser } from './user'
 import { Post, createPost, updatePost, deletePost } from './post'
 import { Comment, createComment, updateComment, deleteComment } from './comment'
 
+// makeSchema defines the GraphQL schema, by combining the GraphQL types defined 
+// by the GraphQL Nexus layer or any manually defined GraphQLType objects
 export const schema = makeSchema({
   types: {
     Query,
@@ -25,10 +27,15 @@ export const schema = makeSchema({
   },
   plugins: [nexusPrisma({ experimentalCRUD: true })],
   outputs: {
+    // GraphQL SDL file generation
     schema: path.join(process.cwd(), 'src/schema.graphql'),
+
+    // TypeScript declaration file generation settings. This file contains types
+    // reflected off the source code
     typegen: path.join(process.cwd(), 'src/nexus.ts'),
   },
   contextType: {
+    // Path to the module where the context type is exported
     module: path.join(process.cwd(), 'src/context.ts'),
     export: 'Context',
   },
