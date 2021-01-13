c509ffc8d55e3fb7d3008487dbe0bc63c80446de
Bernardo-Quina <bernardoquina@gmail.com>
2021-01-13 17:54:48 +0000

    Review website with prisma & nexus: Setup

---
+++ prisma-review-website/src/context.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client'
+
+// Type-safe database client for Typescript & Node.js (ORM replacement)
+const prisma = new PrismaClient({log: ['query']})
+
+export interface Context {
+  prisma: PrismaClient
+}
+
+// Provided to ApolloServer in index.ts
+export const createContext = (): Context => {
+  return { prisma }
+}
---
+++ prisma-review-website/src/schema/schema.ts
@@ -0,0 +1,30 @@
+import { makeSchema } from 'nexus'
+import { nexusPrisma } from 'nexus-plugin-prisma'
+import path from 'path'
+
+import * as userTypes from './user'
+import { Query } from './query'
+
+
+// makeSchema defines the GraphQL schema, by combining the the GraphQL types defined
+// by the GraphQL Nexus layer or any manually defined GraphQLType objects
+export const schema = makeSchema({
+  types: {
+    Query,
+    userTypes,
+  },
+  plugins: [nexusPrisma({ experimentalCRUD: true })],
+  outputs: {
+    // GraphQL SDL file generation
+    schema: path.join(process.cwd(), 'src/schema.graphql'),
+
+    // Typescript declaration file generation settings. This file contains types
+    // reflected off the source code
+    typegen: path.join(process.cwd(), 'src/nexus.ts'),
+  },
+  contextType: {
+    // Path to the module where the context type is exported
+    module: path.join(process.cwd(), 'src/context.ts'),
+    export: 'Context'
+  }
+})
