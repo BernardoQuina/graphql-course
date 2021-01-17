a34d3c20a061822ee76c562a946da235c3707a78
Bernardo-Quina <bernardoquina@gmail.com>
2021-01-17 21:08:38 +0000

    PubsubMany utility added to the bloging api

---
+++ graphql-prisma/src/util/pubsubMany.ts
@@ -0,0 +1,17 @@
+import { Post, Comment, User } from '@prisma/client'
+import { PubSub } from 'apollo-server'
+
+type Payload = {
+  mutation: 'CREATED' | 'UPDATED'| 'DELETED'
+  data: User | Post | Comment
+}
+
+export const pubsubPublishMany = (
+  pubsub: PubSub,
+  stringArr: string[],
+  payload: Payload
+) => {
+  stringArr.forEach((string) => {
+    pubsub.publish(string, payload)
+  })
+}
