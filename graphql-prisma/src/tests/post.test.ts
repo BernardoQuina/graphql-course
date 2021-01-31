74fcd7b1f582c427d028d77752914eb14c9ca7d5
Bernardo-Quina <bernardoquina@gmail.com>
2021-01-31 16:44:22 +0000

    Testing with authentication: posts

--- graphql-prisma/src/tests/post.test.ts
+++ graphql-prisma/src/tests/post.test.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client/core'
 
 import { prisma } from '../context'
 import { getClient } from './utils/getClient'
-import { seedDatabase, userOne } from './utils/seedDatabase'
+import { postOne, seedDatabase, userOne } from './utils/seedDatabase'
 
 const client = getClient()
 
@@ -28,7 +28,7 @@ test('Should only query published post', async () => {
 
 test('Should query my posts, including unpublished ones', async () => {
   const getMyPosts = gql`
-    query MyPostsQuery($userOneId: String!) {
+    query myPostsQuery($userOneId: String!) {
       posts(where: { userId: { equals: $userOneId } }) {
         id
         published
@@ -45,6 +45,81 @@ test('Should query my posts, including unpublished ones', async () => {
 
   expect(response.data.posts.length).toBe(2)
   expect(response.data.posts[1].published).toBe(false) // authenticated only
+})
+
+test('Should be able to update own post', async () => {
+  const updatePost = gql`
+    mutation updatePost($postOneId: String!) {
+      updatePost(
+        whereId: $postOneId
+        updateTitle: "new title"
+        updateBody: "new post body"
+        updatePublished: false
+      ) {
+        title
+        body
+        published
+      }
+    }
+  `
+
+  const client = getClient(userOne.jwt)
+
+  const response = await client.mutate({
+    mutation: updatePost,
+    variables: { postOneId: postOne.post?.id },
+  })
+
+  expect(response.data.updatePost.title).toBe('new title')
+  expect(response.data.updatePost.body).toBe('new post body')
+  expect(response.data.updatePost.published).toBe(false)
+})
+
+test('Should create post', async () => {
+  const createPost = gql`
+    mutation createPost {
+      createPost(
+        title: "created title"
+        body: "created body"
+        published: true
+      ) {
+        title
+        body
+        published
+      }
+    }
+  `
+
+  const client = getClient(userOne.jwt)
+
+  const response = await client.mutate({ mutation: createPost })
+
+  expect(response.data.createPost.title).toBe('created title')
+  expect(response.data.createPost.body).toBe('created body')
+  expect(response.data.createPost.published).toBe(true)
+})
+
+test('Should delete post', async () => {
+  const deletePost = gql`
+    mutation deletePost($postOneId: String!) {
+      deletePost(id: $postOneId) {
+        title
+        body
+        published
+      }
+    }
+  `
+
+  const client = getClient(userOne.jwt)
+
+  await client.mutate({
+    mutation: deletePost,
+    variables: { postOneId: postOne.post?.id },
+  })
+
+  expect(
+    await prisma.post.findUnique({ where: { id: postOne.post?.id } })
+  ).toBe(null)
 })
 
 afterAll(() => {
--- graphql-prisma/src/tests/utils/seedDatabase.ts
+++ graphql-prisma/src/tests/utils/seedDatabase.ts
@@ -1,19 +1,30 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import { User } from '@prisma/client'
+import { Post, User } from '@prisma/client'
 
 import { prisma } from '../../context'
 
 export const userOne: {
-  input: { name: string, email: string, password: string },
-  user?: User,
+  input: { name: string; email: string; password: string }
+  user?: User
   jwt?: string // token
 } = {
   input: {
     name: 'jen',
     email: 'jen@example.com',
     password: bcrypt.hashSync('jenjenjen'),
-  }
+  },
+}
+
+export const postOne: {
+  input: { title: string; body: string; published: boolean }
+  post?: Post
+} = {
+  input: {
+    title: 'dummy post',
+    body: 'dummy post body',
+    published: true,
+  },
 }
 
 export const seedDatabase = async () => {
@@ -24,17 +35,15 @@ export const seedDatabase = async () => {
 
   // create dummy user
   userOne.user = await prisma.user.create({
-    data: userOne.input
+    data: userOne.input,
   })
 
   userOne.jwt = jwt.sign({ userId: userOne.user.id }, process.env.JWT_SECRET)
 
   // create dummy posts
-  await prisma.post.create({
+  postOne.post = await prisma.post.create({
     data: {
-      title: 'dummy post',
-      body: 'dummy post body',
-      published: true,
+      ...postOne.input,
       author: { connect: { id: userOne.user.id } },
     },
   })
